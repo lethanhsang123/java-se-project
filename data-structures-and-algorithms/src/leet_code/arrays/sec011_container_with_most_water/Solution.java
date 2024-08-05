5bf2518506cca0d4fb26f8d840d9fe24e04eda71
sang <lethanhsang14082001@gmail.com>
2024-08-05 07:02:37 +0700

    [feat][dsa][Sec011]

--- data-structures-and-algorithms/src/leet_code/arrays/sec011_container_with_most_water/Solution.java
+++ data-structures-and-algorithms/src/leet_code/arrays/sec011_container_with_most_water/Solution.java
@@ -3,19 +3,22 @@ package leet_code.arrays.sec011_container_with_most_water;
 class Solution {
 
     public static int maxArea(int[] height) {
-        int n = height.length;
-        int maxArea = 0;
-        for (int i = 0; i < n - 2 ; i++) {
-            for (int j = i + 1; j < n - 1; j++) {
-                int area = (j - i) *  (Math.min(j, i));
-                if (area > maxArea) maxArea = area;
-            }
+        int head = 0, tail = height.length - 1, maxArea = 0;
+        while ( head < tail ) {
+            int min = Math.min(height[head], height[tail]) ;
+            maxArea = Math.max (maxArea, (tail - head) * min);
+            while(head < tail && height[head] <= min) head++;
+            while( head < tail && height[tail]<=min) tail--;
         }
+        System.gc();
         return maxArea;
     }
 
     public static void main(String[] args) {
         int[] heigh = new int[] {1,8,6,2,5,4,8,3,7};
-        System.out.println("Result: " + maxArea(heigh));;
+        long current = System.currentTimeMillis();
+        int maxArea = maxArea(heigh);
+        long time = System.currentTimeMillis() - current;
+        System.out.println("Result: " + maxArea + " ; Time: " + time);;
     }
 }
