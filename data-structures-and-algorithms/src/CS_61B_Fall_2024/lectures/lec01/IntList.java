1af84f0417c2481be9fc5252d6d1803269c90b1b
sang <sangle@aggregotoricapaci.com>
2024-09-08 13:52:53 +0700

    [feat][DSA][Update]

---
+++ data-structures-and-algorithms/src/CS_61B_Fall_2024/lectures/lec01/IntList.java
@@ -0,0 +1,34 @@
+package CS_61B_Fall_2024.lectures.lec01;
+
+public class IntList {
+
+    public int first;
+    public IntList rest;
+
+    public IntList(int f, IntList r) {
+        first = f;
+        rest = r;
+    }
+
+    /** Return the size of the list using... recursion! */
+    public int size() {
+        if (rest == null) {
+            return 1;
+        }
+        return 1 + this.rest.size();
+    }
+
+    /** Return the size of the list using no recursion! */
+    public int iterativeSize() {
+        IntList p = this;
+        int totalSize = 0;
+        while (p != null) {
+            totalSize += 1;
+            p = p.rest;
+        }
+        return totalSize;
+    }
+
+
+
+}
