17c5ff373ceb10a3dfa5699c933745f748ab031a
sang <lethanhsang14082001@gmail.com>
2025-01-02 23:39:06 +0700

    [feat][design_pattenr][Observer pattern]

---
+++ design-patterns/src/chapter_02_observer_pattern/observer/ForecastDisplay.java
@@ -0,0 +1,15 @@
+package chapter_02_observer_pattern.observer;
+
+import chapter_02_observer_pattern.DisplayElement;
+
+public class ForecastDisplay implements Observer, DisplayElement {
+    @Override
+    public void display() {
+
+    }
+
+    @Override
+    public void update(float temp, float humidity, float pressure) {
+
+    }
+}
---
+++ design-patterns/src/chapter_02_observer_pattern/observer/StatisticsDisplay.java
@@ -0,0 +1,17 @@
+package chapter_02_observer_pattern.observer;
+
+import chapter_02_observer_pattern.DisplayElement;
+
+public class StatisticsDisplay implements Observer, DisplayElement {
+
+
+    @Override
+    public void display() {
+
+    }
+
+    @Override
+    public void update(float temp, float humidity, float pressure) {
+
+    }
+}
